25f56b3a29a1d3f1acb450fbcb3bed336a3540cd
Gideon Spierings <gideon@nexusmutual.io>
2023-08-04 14:39:57 +0200

    Merge logos package in sdk

---
+++ public/scripts/copy-logos.cjs
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const path = require('node:path');
+const { mkdir, readdir, copyFile } = require('node:fs').promises;
+const { rimraf } = require('rimraf');
+
+/**
+ * Usage:
+ *  node node_modules/@nexusmutual/sdk/dist/scripts/copy-logos.cjs ./public/logos
+ */
+const main = async argv => {
+  if (!argv[2]) {
+    console.error('Please provide a target path');
+    process.exit(1);
+  }
+
+  const targetPath = path.resolve(argv[2]);
+  const sourcePath = path.resolve(__dirname, '../logos-src');
+
+  await rimraf(targetPath);
+  await mkdir(targetPath);
+
+  const dirents = await readdir(sourcePath, { withFileTypes: true });
+  dirents.forEach(dirent => {
+    if (dirent.isFile()) {
+      const sourceFile = path.resolve(sourcePath, dirent.name);
+      const targetFile = path.resolve(targetPath, dirent.name);
+      copyFile(sourceFile, targetFile);
+    }
+  });
+};
+
+main(process.argv);
--- scripts/build-src.js
+++ scripts/build-src.js
@@ -11,12 +11,26 @@ const main = async () => {
   await build({
     entry: ['src/index.ts'],
     format: ['cjs', 'esm'],
+    external: ['react'],
     splitting: false,
     sourcemap: true,
     clean: true,
     dts: true,
     publicDir: 'public',
   });
+
+  // Copy over all processed logo files to dist
+  const outDir = path.join(dist, 'logos');
+  const srcDir = path.join(__dirname, '../generated/logos-src');
+  fs.mkdirSync(outDir);
+  const dirents = await fs.promises.readdir(srcDir, { withFileTypes: true });
+  for (const dirent of dirents) {
+    if (dirent.isFile()) {
+      const source = path.join(srcDir, dirent.name);
+      const dest = path.join(outDir, dirent.name);
+      fs.copyFileSync(source, dest);
+    }
+  }
 };
 
 main()

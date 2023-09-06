0c0350ac41f9bbade2be5a6dceb512a1424eba67
Gideon Spierings <gideon@nexusmutual.io>
2023-09-06 13:27:56 +0200

    Fix bug where logo types were not included in dist

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export * from '@nexusmutual/deployments';
 // Export product data so it will be included in the bundle
 export { default as products } from '../generated/products.json';
 export { default as productTypes } from '../generated/product-types.json';
+
+// Export generated logo types
+export * from '../generated/logos/types';

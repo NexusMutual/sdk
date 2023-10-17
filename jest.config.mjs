858caacad188e5e18f6e1d3170257a5098f74f5e
Gideon Spierings <gideon@nexusmutual.io>
2023-10-17 14:50:22 +0200

    Setup jest

---
+++ jest.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
+
+export default config;
---
+++ src/swap/ethToNxm.test.ts
@@ -0,0 +1,5 @@
+describe('ethToNxm', () => {
+  it('should return the correct amount of NXM', () => {
+    expect(true).toBe(true);
+  });
+});

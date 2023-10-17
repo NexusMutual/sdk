3d30fc448ed59f2022db18c389b48202ec689098
Gideon Spierings <gideon@nexusmutual.io>
2023-10-17 15:50:04 +0200

    Update tokenomics functions to accept only bigints

--- src/swap/ethToNxm.ts
+++ src/swap/ethToNxm.ts
@@ -1,15 +1,12 @@
 import { Reserves } from './reserves.type';
 
-export const ethToNxm = (ethAmount: bigint | string, reserves: Reserves): bigint => {
+export const ethToNxm = (ethIn: bigint, reserves: Reserves): bigint => {
   // Extracting the current reserves of Nxm and Eth
   const nxmReservesBefore = BigInt(reserves.nxmA);
   const ethReserveBefore = BigInt(reserves.ethReserve);
 
   // Calculate the constant product (k) for the market maker model
   const k = ethReserveBefore * nxmReservesBefore;
-
-  // Amount of Eth that will flow into the pool
-  const ethIn = BigInt(ethAmount);
 
   // Simulate the swap and calculate the new reserves
   const ethReservesAfter = ethReserveBefore + ethIn;

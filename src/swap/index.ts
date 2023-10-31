2ea84dd55d4ad2d453ebcad8b583d0b3b2b52797
Andreea <9848598+mixplore@users.noreply.github.com>
2023-10-31 16:24:05 +0200

    Export spot price and price impact methods

--- src/swap/index.ts
+++ src/swap/index.ts
@@ -3,3 +3,7 @@ export * from './calculateExactEthForNxm';
 export * from './calculateNxmForExactEth';
 export * from './calculateEthForExactNxm';
 export * from './reserves.type';
+export * from './calculatePriceImpactA';
+export * from './calculatePriceImpactB';
+export * from './calculateSpotPrice';
+

76de0c85673b361930247ea7c3c930a0a305cc89
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-04-08 18:32:13 +0300

    Add types directory

---
+++ src/types/data.ts
@@ -0,0 +1,19 @@
+/**
+ * 0x prefixed 20 bytes ethereum address encoded as hex string
+ */
+export type Address = string;
+
+/**
+ * Integer value, used to represent whole numbers without fractional parts
+ */
+export type Integer = number;
+
+/**
+ * Integer values in string format
+ */
+export type IntString = string;
+
+/**
+ * 0x prefixed hexadecimal string with even length, ensuring valid byte pairs.
+ */
+export type DataHexString = string;

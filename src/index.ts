d2ca76044d628190dad099ab3d32cec0983a87d3
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-04-10 13:21:56 +0300

    Remove comments

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,8 @@ export * from './swap';
 
 export * from './buyCover';
 
-//
 export * from './types';
 
 export * from './quote';
 
-// TODO: export under sdk.constants
 export * from './constants';

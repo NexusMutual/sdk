8f66a9d38385845ea88351e8979a66e07f4e9641
Miljan Milidrag <miljan.milidrag@protonmail.com>
2024-06-04 11:33:08 +0200

    Add a function to calculate product cover assets

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,5 +1,11 @@
 const path = require('path');
 
+/**
+ * Returns assets array from the integer representation of bit mapping .
+ *
+ * @param {Integer} coverAsset - The assets that are supported by the product.
+ * @return {String[]} Returns assets array.
+ */
 const parseProductCoverAssets = coverAssets => {
   const COVER_ASSETS_MAP = {
     0: 'ETH',
@@ -25,6 +31,20 @@ const parseProductCoverAssets = coverAssets => {
   return productCoverAssets;
 };
 
+/**
+ * Calculate the bit mapping for the product cover assets.
+ *
+ * @param {Integer[]} coverAssets - The assets that are supported by the product.
+ * @return {Integer} Returns integer representation of bit mapping for product cover assets.
+ */
+const parseCoverAssetToProductAsset = coverAssets => {
+  if (coverAssets.length === 0) {
+    return 0;
+  }
+
+  return coverAssets.reduce((acc, coverAsset) => acc + Math.pow(2, coverAsset), 0);
+};
+
 const parseFilePath = filePath => {
   const fileName = path.basename(filePath);
   const regex = /(\d+)?-?(.+?)\.(\w+$)/;
@@ -36,5 +56,6 @@ const parseFilePath = filePath => {
 
 module.exports = {
   parseProductCoverAssets,
+  parseCoverAssetToProductAsset,
   parseFilePath,
 };

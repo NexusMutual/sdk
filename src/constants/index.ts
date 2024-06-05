3e6fa40c416e9c8ad11754a692b056383ee72277
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-06-05 14:33:59 +0300

    Export allPrivateProductsIds constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,5 @@
+const { allPrivateProductsIds } = require('./privateProducts') as { allPrivateProductsIds: number[] };
+
+export { allPrivateProductsIds };
 export * from './buyCover';
 export * from './products';

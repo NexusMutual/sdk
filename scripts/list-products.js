4d05e5b4659e76e42ef58adca61febfade8b17d0
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-06-27 17:26:58 +0300

    Update list:products script to support optional argument

--- scripts/list-products.js
+++ scripts/list-products.js
@@ -9,7 +9,14 @@ if (!process.env.PROVIDER_URL) {
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER_URL);
 
+/**
+ * Asynchronously lists products with optional argument to list only the last N products
+ *
+ * npm run list:products // lists all products
+ * npm run list:products 20 // lists the last 20 products
+ */
 const listProducts = async () => {
+  const count = process.argv[2];
   const abi = deployments.Cover;
   const address = deployments.addresses.Cover;
   if (!abi || !address) {
@@ -19,11 +26,21 @@ const listProducts = async () => {
   console.log('Fetching products...');
 
   const cover = new ethers.Contract(address, abi, provider);
-  const products = await cover.getProducts();
-  const productNames = await Promise.all(products.map((_, i) => cover.productNames(i)));
+  const productsCount = (await cover.getProducts()).length;
 
+  let productIds;
+  if (count) {
+    // grab the last n items equal to count
+    const start = Math.max(0, productsCount - parseInt(count));
+    const end = productsCount;
+    productIds = Array.from({ length: end - start }, (_, i) => i + start);
+  } else {
+    productIds = Array.from({ length: productsCount }, (_, i) => i);
+  }
+
+  const products = await Promise.all(productIds.map(async id => ({ id, name: await cover.productNames(id) })));
   console.log('Products: \n');
-  productNames.forEach((productName, i) => console.log(i, productName));
+  products.forEach(product => console.log(product.id, product.name));
 };
 
 listProducts();

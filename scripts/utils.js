ac85f322dfb822481e19a3cb89e96b64a489a414
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-11-13 10:51:43 +0200

    feat: fetch latest coverAssets mapping data from on-chain

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,21 +1,51 @@
-const path = require('path');
+const path = require('node:path');
 
+const { addresses, Pool } = require('@nexusmutual/deployments');
+const ethers = require('ethers');
+
+/**
+ * Retrieves the latest on-chain coverAssets IDs to their corresponding token symbols mapping.
+ *
+ * @param {ethers.providers.Provider} provider - The Ethereum provider to interact with the blockchain.
+ * @returns {Promise<Object>} A promise that resolves to an object mapping cover asset IDs to their symbols.
+ */
+const getCoverAssetsMap = async provider => {
+  const BASE_COVER_ASSETS_MAP = { 0: 'ETH', 255: 'NXM' };
+
+  const pool = new ethers.Contract(addresses.Pool, Pool, provider);
+  const assets = await pool.getAssets();
+  console.log('assets: ', assets);
+
+  // Filter for cover assets and exclude ETH address, but keep track of original index
+  const coverAssets = assets
+    .map((asset, index) => ({ ...asset, assetId: index }))
+    .filter(
+      ({ 0: address, 1: isCoverAsset }) => isCoverAsset && address !== '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+    );
+
+  // Get symbol for each cover asset and use original index as key
+  for (const { 0: address, assetId } of coverAssets) {
+    const token = new ethers.Contract(address, ['function symbol() view returns (string)'], provider);
+    const symbol = await token.symbol();
+
+    if (!symbol) {
+      throw new Error(`Failed to get symbol for token at address ${address}`);
+    }
+
+    BASE_COVER_ASSETS_MAP[assetId] = symbol;
+  }
+
+  return BASE_COVER_ASSETS_MAP;
+};
 /**
  * Returns assets array from the integer representation of bit mapping .
  *
  * @param {Integer} coverAsset - The assets that are supported by the product.
  * @return {String[]} Returns assets array.
  */
-const parseProductCoverAssets = coverAssets => {
-  const COVER_ASSETS_MAP = {
-    0: 'ETH',
-    1: 'DAI',
-    6: 'USDC',
-    255: 'NXM',
-  };
-
+const parseProductCoverAssets = (coverAssets, coverAssetsMap) => {
   if (coverAssets === 0) {
-    return [COVER_ASSETS_MAP[0], COVER_ASSETS_MAP[1], COVER_ASSETS_MAP[6]];
+    return [coverAssetsMap[0], coverAssetsMap[1], coverAssetsMap[6], coverAssetsMap[7]];
   }
 
   const coverAssetsBinary = coverAssets.toString(2);
@@ -24,7 +54,7 @@ const parseProductCoverAssets = coverAssets => {
 
   coverAssetsArray.forEach((bit, coverAssetId) => {
     if (bit === '1') {
-      productCoverAssets.push(COVER_ASSETS_MAP[coverAssetId]);
+      productCoverAssets.push(coverAssetsMap[coverAssetId]);
     }
   });
 
@@ -58,4 +88,5 @@ module.exports = {
   parseProductCoverAssets,
   parseCoverAssetToProductAsset,
   parseFilePath,
+  getCoverAssetsMap,
 };

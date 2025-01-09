81020ddd3ee975153f3e5a19216d8c94429583c1
mixplore <9848598+mixplore@users.noreply.github.com>
2025-01-10 01:22:53 +0200

    refactor: ts types and validation for ipfs content

--- src/types/ipfs.ts
+++ src/types/ipfs.ts
@@ -2,6 +2,7 @@ export enum ContentType {
   coverValidators = 'coverValidators',
   coverQuotaShare = 'coverQuotaShare',
   coverAumCoverAmountPercentage = 'coverAumCoverAmountPercentage',
+  coverWalletAddress = 'coverWalletAddress',
   coverWalletAddresses = 'coverWalletAddresses',
   coverFreeText = 'coverFreeText',
   // ---------------------------------------------------------
@@ -12,26 +13,97 @@ export enum ContentType {
   governanceCategory = 'governanceCategory',
 }
 
-export const DEFAULT_VERSION = '1.0';
+type CoverValidators = {
+  version: '1.0';
+  validators: string[];
+};
 
-export interface IPFSDataTypes {
-  [ContentType.coverValidators]: { validators: string[] };
-  [ContentType.coverQuotaShare]: { quotaShare: number };
-  [ContentType.coverAumCoverAmountPercentage]: { aumCoverAmountPercentage: number };
-  [ContentType.coverWalletAddresses]: { walletAddresses: string[] };
-  [ContentType.coverFreeText]: { freeText: string };
-  //   ---------------------------------------------------------
-  [ContentType.stakingPoolDetails]: { poolName: string; poolDescription: string };
-  [ContentType.claimProof]: {
-    coverId: number;
-    affectedAddresses: string[];
-    affectedChain: string;
-    incidentDescription: string;
-    incidentTransactionHashes: string[];
-    incidentEvidenceLinks: string[];
-    attachedFilesHashes: string[];
-  };
-  [ContentType.assessmentCriteriaAnswers]: { answers: Record<string, string> };
-  [ContentType.governanceProposal]: { proposal: string };
-  [ContentType.governanceCategory]: { category: string };
-}
+type CoverQuotaShare = {
+  version: '1.0';
+  quotaShare: number;
+};
+
+type CoverAumCoverAmountPercentage = {
+  version: '1.0';
+  aumCoverAmountPercentage: number;
+};
+
+type CoverWalletAddress = {
+  version: '1.0';
+  walletAddress: string;
+};
+
+type CoverWalletAddresses =
+  | {
+      version: '1.0';
+      walletAddresses: string;
+    }
+  | {
+      version: '2.0';
+      walletAddresses: string[];
+    };
+
+type CoverFreeText = {
+  version: '1.0';
+  freeText: string;
+};
+
+type StakingPoolDetails = {
+  version: '1.0';
+  poolName: string;
+  poolDescription: string;
+};
+
+type ClaimProof = {
+  version: '1.0';
+  coverId: number;
+  affectedAddresses: string[];
+  affectedChain: string;
+  incidentDescription: string;
+  incidentTransactionHashes: string[];
+  incidentEvidenceLinks: string[];
+  attachedFilesHashes: string[];
+};
+
+type AssessmentCriteriaAnswers = {
+  version: '1.0';
+  answers: Record<string, string>;
+};
+
+type GovernanceProposal = {
+  version: '1.0';
+  proposal: string;
+};
+
+type GovernanceCategory = {
+  version: '1.0';
+  category: string;
+};
+
+export type IPFSContentTypes =
+  | CoverValidators
+  | CoverQuotaShare
+  | CoverAumCoverAmountPercentage
+  | CoverWalletAddress
+  | CoverWalletAddresses
+  | CoverFreeText
+  // ---------------------------------------------------------
+  | StakingPoolDetails
+  | ClaimProof
+  | AssessmentCriteriaAnswers
+  | GovernanceProposal
+  | GovernanceCategory;
+
+export type IPFSContentAndType =
+  | [type: ContentType.coverValidators, content: CoverValidators]
+  | [type: ContentType.coverQuotaShare, content: CoverQuotaShare]
+  | [type: ContentType.coverAumCoverAmountPercentage, content: CoverAumCoverAmountPercentage]
+  | [type: ContentType.coverWalletAddress, content: CoverWalletAddress]
+  | [type: ContentType.coverWalletAddresses, content: CoverWalletAddresses]
+  | [type: ContentType.coverFreeText, content: CoverFreeText]
+  // ---------------------------------------------------------
+  | [type: ContentType.stakingPoolDetails, content: StakingPoolDetails]
+  | [type: ContentType.claimProof, content: ClaimProof]
+  | [type: ContentType.assessmentCriteriaAnswers, content: AssessmentCriteriaAnswers]
+  | [type: ContentType.governanceProposal, content: GovernanceProposal]
+  | [type: ContentType.governanceCategory, content: GovernanceCategory];

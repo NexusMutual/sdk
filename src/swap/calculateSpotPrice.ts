e126da52d98ef57005391e28799dab95a204c55d
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2025-03-20 15:05:49 +0200

    feat: wip NexusSDK class

--- src/swap/calculateSpotPrice.ts
+++
@@ -1,9 +0,0 @@
-import { Reserves } from './reserves.type';
-
-export const calculateSpotPrice = (reserves: Reserves) => {
-  const oneEth = BigInt(1e18);
-  return {
-    spotPriceA: (oneEth * reserves.ethReserve) / reserves.nxmA,
-    spotPriceB: (oneEth * reserves.ethReserve) / reserves.nxmB,
-  };
-};

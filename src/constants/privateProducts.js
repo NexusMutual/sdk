abd06e93aa41b82f9b092ec31ba252fca9835553
ssbarbee <ssbarbee@users.noreply.github.com>
2025-04-01 17:37:29 +0200

    chore: name

--- src/constants/privateProducts.js
+++ src/constants/privateProducts.js
@@ -50,7 +50,7 @@ const allPrivateProductsIds = [
   260, // DeFi Covered
   273, // L1 Advisors Cover
   274, // Figment ETH Slashing
-  291, // Ouroboros
+  291, // OC Cover
 ];
 
 module.exports = {
